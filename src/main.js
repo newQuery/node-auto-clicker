d0a301caf3832f5eba32692f80b9ce3303a5f664
newQuery <wilmshorst.tom@gmail.com>
2023-01-29 21:13:00 +0100

    Show running config instead of showing default keybinds

--- src/main.js
+++ src/main.js
@@ -12,10 +12,11 @@ ${chalk.green.bold('NodeJS Autoclicker')}
 
 Configuration in ${chalk.italic.underline('./config.json')}
 
-${chalk.bold('Default hotkeys')}
-Start:               ${chalk.bold('S')}
-Pause:               ${chalk.bold('P')}
-Stop (exit process): ${chalk.bold('T')}
+${chalk.bold('Current configuration')}
+Start:                           ${chalk.bold(config.startKey.toUpperCase())}
+Pause:                           ${chalk.bold(config.pauseKey.toUpperCase())}
+Stop (exit process):             ${chalk.bold(config.stopKey.toUpperCase())}
+Milliseconds between each click: ${chalk.bold(config.millisecondsBetweenEachClick)}
 `);
 
 const v = new GlobalKeyboardListener();
@@ -24,18 +25,15 @@ let interval;
 
 v.addListener(function (e, down) {
     if(e.state.toLocaleLowerCase() === 'down' && e.name.toLocaleLowerCase() === config.startKey.toLocaleLowerCase()) {
-        console.log('[AUTOCLICKER] Starting autoclicker');
         interval = setInterval(() => robot.mouseClick(config.mouseButton), config.millisecondsBetweenEachClick);
     }
 
     if(e.state.toLocaleLowerCase() === 'down' && e.name.toLocaleLowerCase() === config.stopKey.toLocaleLowerCase()) {
-        console.log("[AUTOCLICKER] Stopping and exit process");
         clearInterval(interval)
         process.exit()
     }
 
     if(e.state.toLocaleLowerCase() === 'down' && e.name.toLocaleLowerCase() === config.pauseKey.toLocaleLowerCase()) {
-        console.log("[AUTOCLICKER] Pausing autoclick");
         clearInterval(interval)
     }
 });
